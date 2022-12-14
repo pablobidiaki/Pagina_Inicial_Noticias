74883d2832e39e36f742eb321528fe3444bd423c
pablobidiaki <pablobidiaki@hotmail.com>
2022-12-14 18:39:31 -0300

    projeto mobile finalizado

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+var contagem = 0;
 function menuHamburguer(){
-    document.getElementById('burguer').classList.toggle('visivel')
+    contagem += 1;
 
+    if(contagem%2 == 0){
+        document.getElementById('burguer').classList.add('visivel')
+        document.querySelector('main').classList.remove('teste')
+        document.querySelector('header').classList.remove('teste')
 
+    }else{
+        document.getElementById('burguer').classList.remove('visivel')
+        document.querySelector('main').classList.add('teste')
+        document.querySelector('header').classList.add('teste')
+    }
 }
+
+
